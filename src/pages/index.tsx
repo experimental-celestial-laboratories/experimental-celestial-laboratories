14e32bcd7c41b4fe22cd58a32f310f7a003e4a24
Hri7566 <hri7566@gmail.com>
2023-07-07 08:08:11 -0400

    update description

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,7 +9,7 @@ export default function Home() {
     <>
       <Head>
         <title>XCL</title>
-        <meta name="description" content="XCL's homepage" />
+        <meta name="description" content="XCL homepage" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       {/* <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#993800] to-[#250e00]"> */}
