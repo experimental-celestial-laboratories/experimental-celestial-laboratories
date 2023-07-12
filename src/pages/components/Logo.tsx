8d745ba7538e0854dc36d01bbd90c4e38f2dc8d1
Hri7566 <hri7566@gmail.com>
2023-07-12 10:53:15 -0400

    abstract logo

---
+++ src/pages/components/Logo.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export const Logo = () => {
+  return (
+    <div id="logo">
+      <Image src="/xcl.png" alt="XCL" width={209} height={105} />
+    </div>
+  );
+};
