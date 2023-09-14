d48050242ffcd61daf8da750a916c2d1b863a5be
Hri7566 <hri7566@gmail.com>
2023-09-14 00:32:45 -0400

    Add random backgrounds to all pages

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -8,7 +8,21 @@ import { LinkButton } from "../components/LinkButton";
 import { NavigationBar } from "../components/NavigationBar";
 import { PageContentBox } from "~/components/PageContentBox";
 
-export default function Home() {
+export default function About() {
+  const bgs = [
+    "/bg/webp/bg1.webp",
+    "/bg/webp/bg2.webp",
+    "/bg/webp/bg3.webp",
+    "/bg/webp/bg4.webp",
+    "/bg/webp/bg5.webp",
+    "/bg/webp/bg6.webp",
+    "/bg/webp/bg7.webp",
+    "/bg/webp/bg8.webp",
+    "/bg/webp/bg9.webp",
+  ];
+
+  const randomBg = bgs[Math.floor(Math.random() * bgs.length)] as string;
+
   return (
     <>
       <Head>
@@ -17,7 +31,10 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       {/* <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#993800] to-[#250e00]"> */}
-      <main className="bg-[url(/bg/bg_day.png)] bg-cover bg-center">
+      <main
+        className="bg-cover bg-center"
+        style={{ backgroundImage: `url("${randomBg}")` }}
+      >
         <div className="bg-black/60">
           <div className="flex min-h-screen flex-col items-center justify-center bg-orange-800/40">
             <Link className="absolute left-0 top-0" href="/">
