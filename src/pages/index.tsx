ec62bdd4e3651de71addeb3569b3fb856b3c2029
Hri7566 <hri7566@gmail.com>
2024-08-01 17:40:46 -0400

    Hide team page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,7 +11,7 @@ export default function Home() {
         </div>
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
           <Logo />
-          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
+          <div className="grid grid-cols-1 gap-4 sm:grid-cols-1 md:gap-8">
             <LinkButton
               href="https://git.hri7566.info/xcl"
               title="Git →"
